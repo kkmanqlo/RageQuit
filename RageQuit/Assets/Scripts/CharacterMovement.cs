cf3dbe24977c9a498f9544b35b3ef1c1e67cebcc
kkmanqlo <jomech23@bemen3.cat>
2025-04-07 20:09:39 +0200

    Personaje con animaciones principales (Iddle y Run)

---
+++ RageQuit/Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,60 @@
+using System;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    //Metodos públicos para que se puedan modificar desde el editor
+    public float Speed;
+    public float JumpForce;
+
+    //Metodos privados para que no se puedan modificar desde el editor
+    private Rigidbody2D Rigidbody2D;
+    private Animator Animator;
+    private float Horizontal;
+    private bool Grounded;
+
+    // Se llama al inicio del juego
+    void Start()
+    {
+        Rigidbody2D = GetComponent<Rigidbody2D>();
+        Animator = GetComponent<Animator>();
+
+    }
+
+    // Se llama una vez por frame para actualizar la lógica del juego
+    void Update()
+    {
+        Horizontal = Input.GetAxisRaw("Horizontal");
+
+        if (Horizontal < 0.0f) transform.localScale = new Vector3(-1.0f, 1.0f, 1.0f);
+        else if (Horizontal > 0.0f) transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+
+        Animator.SetBool("running", Horizontal != 0.0f);
+
+        Debug.DrawRay(transform.position, Vector3.down * 0.16f, Color.red);
+        if (Physics2D.Raycast(transform.position, Vector3.down, 0.16f))
+        {
+            Grounded = true;
+        }
+        else Grounded = false;
+        
+
+        if (Input.GetKeyDown(KeyCode.W) && Grounded)
+        {
+            Jump();
+        }
+    }
+
+    //Metodo para saltar
+    private void Jump()
+    {
+        Rigidbody2D.AddForce(Vector2.up * JumpForce);
+    }
+
+    //El metodo FixedUpdate se llama en cada frame de fisica
+
+    private void FixedUpdate()
+    {
+        Rigidbody2D.linearVelocity = new Vector2(Horizontal, Rigidbody2D.linearVelocity.y);
+    }
+}
